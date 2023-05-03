38d0aa90df473378a3d517fac7ec906d4c6aceb7
DenisovPavel <Waste11@mail.ru>
2023-05-03 18:29:47 +0300

    all checked

---
+++ src/main/java/Lec4/Ex003.java
@@ -0,0 +1,21 @@
+package Lec4;
+
+import java.util.*;
+
+public class Ex003 {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(1); //+1
+        deque.addLast(2); //+2
+        deque.removeLast(); //-2
+        deque.offerFirst(1); //+1 [ 1 , 1(field 6) ]
+        deque.offerLast(2); // + 2 [1 ,1 ,2 ]
+        deque.pollFirst(); // -1
+        deque.pollLast();
+        deque.getFirst();
+        deque.getLast();
+        deque.peekFirst();
+        deque.peekLast();
+        System.out.println(deque);
+    }
+}

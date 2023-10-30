49d4a4efeb8c1fcc7ce87528b90c5172d5017365
DenisovPavel <Waste11@mail.ru>
2023-10-30 11:52:42 +0300

    Solid Principles Teachers In Group

--- src/main/java/ComplexNumberCalculator/Controller/CalculateInterface.java
+++ src/main/java/ComplexNumberCalculator/Controller/CalculateInterface.java
@@ -4,7 +4,8 @@ import ComplexNumberCalculator.Model.Calculator;
 
 /**
  * @params CalculateInterface интерфейс для реализации методов;
- * {@link }
+ * {@link <a href="https://github.com/DenisovPavel/Java-Core-SE/blob/main/src/main/java
+ * /ComplexNumberCalculator/Controller/CalculatorDecorator.java">...</a>}
  */
 public interface CalculateInterface {
     Calculator additionNumbers(Calculator numberNew);

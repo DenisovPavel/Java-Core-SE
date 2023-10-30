ee1f2bed0e352f18399624c7bc2d9324fd7545f6
DenisovPavel <Waste11@mail.ru>
2023-10-30 12:00:40 +0300

    Solid Principles Teachers In Group

--- src/main/java/ComplexNumberCalculator/Controller/CalculateInterface.java
+++ src/main/java/ComplexNumberCalculator/Controller/CalculateInterface.java
@@ -4,10 +4,10 @@ import ComplexNumberCalculator.Model.Calculator;
 
 /**
  * @params CalculateInterface интерфейс для реализации методов;
- * {@see CalculatorDecorator #additionNumbers(Calculator numberNew)  }
- * {@see CalculatorDecorator #subtractNumbers(Calculator numberNew)  }
- * {@see CalculatorDecorator #multiplyNumbers(Calculator numberNew)  }
- * {@see CalculatorDecorator #Calculator divideNumbers(Calculator numberNew);  }
+ * @see CalculatorDecorator #additionNumbers(Calculator numberNew);
+ * @see CalculatorDecorator #subtractNumbers(Calculator numberNew);
+ * @see CalculatorDecorator #multiplyNumbers(Calculator numberNew);
+ * @see CalculatorDecorator #Calculator divideNumbers(Calculator numberNew);
  */
 public interface CalculateInterface {
     Calculator additionNumbers(Calculator numberNew);
